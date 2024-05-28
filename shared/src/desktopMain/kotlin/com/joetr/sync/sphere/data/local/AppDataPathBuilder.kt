fd59db892c01b309a392ac65f3d216c07f01822b
Joe Roskopf <7951665+j-roskopf@users.noreply.github.com>
2024-05-28 16:23:02 -0500

    desktop release

---
+++ shared/src/desktopMain/kotlin/com/joetr/sync/sphere/data/local/AppDataPathBuilder.kt
@@ -0,0 +1,26 @@
+package com.joetr.sync.sphere.data.local
+
+import java.io.File
+
+internal object AppDataPathBuilder {
+
+    private val appDataPath = when {
+        System.getProperty("os.name").contains("Mac", true) -> {
+            "${System.getProperty("user.home")}/Library/Application Support/FeedFlow"
+        }
+        System.getProperty("os.name").contains("windows", true) -> {
+            "${System.getProperty("user.home")}\\AppData\\Local\\FeedFlow"
+        }
+        else -> {
+            error("This type OS not implemented")
+        }
+    }
+
+    fun getAppDataPath(): String {
+        val appPath = appDataPath
+        if (!File(appPath).exists()) {
+            File(appPath).mkdirs()
+        }
+        return appPath
+    }
+}
--- shared/src/desktopMain/kotlin/com/joetr/sync/sphere/data/local/DriverFactory.kt
+++ shared/src/desktopMain/kotlin/com/joetr/sync/sphere/data/local/DriverFactory.kt
@@ -4,12 +4,19 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.joetr.sync.sphere.SyncSphereRoomDatabase
 import com.joetr.sync.sphere.data.RoomConstants
+import java.io.File
+import java.util.Properties
 
 actual class DriverFactory {
     actual fun createDriver(): SqlDriver {
-        val driver = JdbcSqliteDriver("jdbc:sqlite:${RoomConstants.DATABASE_NAME}")
+        val appPath = AppDataPathBuilder.getAppDataPath()
+
+        val databasePath = File(appPath, "/${RoomConstants.DATABASE_NAME}")
+
+        val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY + databasePath.absolutePath, Properties())
 
         SyncSphereRoomDatabase.Schema.create(driver)
+
         return driver
     }
 }
