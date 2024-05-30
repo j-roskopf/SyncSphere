1cbbaa72fa1f269ef5998d1978f2c0b66d5b4fbf
Joe Roskopf <7951665+j-roskopf@users.noreply.github.com>
2024-05-30 15:01:16 -0500

    sdfdf

--- shared/src/commonMain/kotlin/com/joetr/sync/sphere/ui/new/NewRoomScreenModel.kt
+++ shared/src/commonMain/kotlin/com/joetr/sync/sphere/ui/new/NewRoomScreenModel.kt
@@ -76,7 +76,7 @@ class NewRoomScreenModel(
                     }
                 },
                 onFailure = {
-                    _state.value = NewRoomState.Error(it.cause.toString())
+                    _state.value = NewRoomState.Error(it.message.orEmpty())
                 },
             )
         }
--- shared/src/desktopMain/kotlin/com/joetr/sync/sphere/data/GoogleFirebaseApi.kt
+++ shared/src/desktopMain/kotlin/com/joetr/sync/sphere/data/GoogleFirebaseApi.kt
@@ -34,7 +34,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.patch
@@ -58,7 +57,6 @@ class GoogleFirebaseApi {
     @OptIn(ExperimentalSerializationApi::class)
     private val client by lazy {
         HttpClient {
-            install(Logging)
             defaultRequest {
                 url(BASE_URL)
                 contentType(ContentType.Application.Json)
