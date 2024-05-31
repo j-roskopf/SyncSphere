7cf1f1fdb2f6d6f80ef737b3a431416b020a1bca
Joe Roskopf <7951665+j-roskopf@users.noreply.github.com>
2024-05-30 19:23:00 -0500

    versions

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -42,7 +42,7 @@ compose.desktop {
 
             targetFormats(TargetFormat.Dmg, TargetFormat.Pkg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "Sync Sphere"
-            packageVersion = "1.6.5"
+            packageVersion = "1.7.0"
 
             macOS {
                 iconFile.set(project.file("src/jvmMain/resources/icon/desktop_icon.icns"))
